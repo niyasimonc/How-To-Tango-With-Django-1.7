1020ee8cb922da74ae009d62d35d9ba20b85ef42
Niya Simon C <niyasimonc@gmail.com>
2016-09-06 23:40:35 +0530

    firat commit

--- Chapter 5/rango/views.py
+++ Chapter 5/rango/views.py
@@ -11,3 +11,16 @@ def index(request):
     # Note that the first parameter is the template we wish to use.
 
     return render(request, 'rango/index.html', context_dict)
+
+
+def about(request):
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
+    context_dict = {'boldmessage': "I am bold font from the context for about section"}
+
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+
+    return render(request, 'rango/about.html', context_dict)
+
